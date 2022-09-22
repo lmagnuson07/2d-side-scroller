939c85d44e890e3a67807bacdb607ecb8d1a592b
lmagnuson07 <logmagns07@gmail.com>
2022-09-21 20:26:36 -0600

    Project setup complete

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello Vite!</h1>
+    <div>
+      <button type="button"></button>
+    </div>
+    <p>
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
