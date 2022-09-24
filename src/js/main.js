308ee5b9a1c2f9b255757ee36c8d05a9c70e97ac
lmagnuson07 <logmagns07@gmail.com>
2022-09-24 00:46:09 -0600

    First 4 tasks of initial setup issue complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,24 @@
+window.addEventListener('load', function(){
+    // hides loading message
+    const loading = document.getElementById('loading');
+    loading.style.display = 'none';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-    <div>
-      <button type="button"></button>
-    </div>
-    <p>
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
+    // canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1400;
+    canvas.height = 720;
+    
+    // implements the full screen functionality 
+    const fullScreenBtn = document.getElementById('fullScreenBtn');
+    const toggleFullScreen = function toggleFullScreen(){
+        if (!document.fullScreenElement){
+            canvas.requestFullscreen().catch(err => {
+                alert(`Error, can't enable full-screen mode: ${err.message}`);
+            })
+        } else {
+            document.exitFullscreen();
+        }
+    }
+    fullScreenBtn.addEventListener('click', toggleFullScreen);
+});
